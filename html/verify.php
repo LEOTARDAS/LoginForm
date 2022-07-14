8e51ba512a8980ecd6399a86afc6d4f05ae9aa69
Leonardas <leonardas.varanavicius@gmail.com>
2022-07-14 16:30:40 +0300

    added login verification from database

--- html/verify.php
+++ html/verify.php
@@ -1,18 +1,37 @@
-"<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <body>
 
 <?php
+
 	$username = $_POST["username"];
 	$password = $_POST["password"];
-	if($username=="admin" && $password=="admin123")
+	define('DB_SERVER', 'localhost');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', '');
+	define('DB_NAME', 'loginform');
+
+	$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+	if(!$conn)
 	{
-		header("location: dashboard.php");
-	} 
-	else
-	{
-		header("location: index.php");
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 	}
+
+	$sqlU = "SELECT username,password FROM users";
+	$resultU = mysqli_query($conn, $sqlU);
+
+	while($row = mysqli_fetch_assoc($resultU)){
+		$tempU = $row["username"];
+		$tempP = $row["password"];
+
+		if($username==$tempU && $password==$tempP)
+		{
+			header("location: dashboard.php");
+			die();
+		} 
+	}
+	header("location: index.php");
 ?>
 
 
